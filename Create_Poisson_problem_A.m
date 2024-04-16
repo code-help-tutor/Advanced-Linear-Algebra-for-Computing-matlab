9d37bd5e23e8cb0c0d1be96f77c16e1114100988
Code Help Tutor <1823890830@qq.com>
2024-04-16 01:01:18 +0000

    WeChat cstutorcs Advanced Linear Algebra for Computing matlab

---
+++ Create_Poisson_problem_A.m
@@ -0,0 +1,20 @@
+function [ A ] = Create_Poisson_problem_A( N )
+
+  % Create the archtypical matrix A for an N x N Poisson problem (5-point
+  % stencil.
+
+  % Set the diagonal
+
+  
+  % Set the entries of the first sub and super diagonals
+
+  
+  % Set the other off-diagonal entries
+
+  
+end
+
+
+
+
+
